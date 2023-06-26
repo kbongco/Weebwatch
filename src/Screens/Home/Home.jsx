d149a5aa8c7389dc21e0e8f2ade61176c1b5d978
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-06-26 12:38:45 -0400

    added text and button

--- src/Screens/Home/Home.jsx
+++ src/Screens/Home/Home.jsx
@@ -1,10 +1,20 @@
-import { Flex, Spacer, Button, Center, Text } from '@chakra-ui/react'
+import { Flex, Spacer, Button, Center, Text, Box } from '@chakra-ui/react'
 
 export default function Home() {
 
   return (
-    <Flex bg='#FFBCB3' h='90vh' justify='center'>
-       <Text fontSize='2rem'>WeebWatch</Text>
+    <>
+    <Flex bg='#FFBCB3' h='90vh' align='center' flexDirection='column'>
+        <Text fontSize='2rem' pt='64px'>My Weeb List</Text>
+        <Box>
+          <Flex>
+            <Text fontSize='2rem' pt='24px' align='center'>A fun way to track anime you've been watching</Text>
+          </Flex>
+        </Box>
+        <Box pt='24px'>
+          <Button colorScheme='orange' size='lg'>Add some Anime here!</Button>
+          </Box>
     </Flex>
+    </>
   )
 }
