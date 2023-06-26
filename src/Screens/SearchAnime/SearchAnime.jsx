f10d5da9d653307d132707993903f93e69709442
Kathleen Bongco <kathleen.bongco@gmail.com>
2023-06-26 14:50:37 -0400

    starting point

--- src/Screens/SearchAnime/SearchAnime.jsx
+++ src/Screens/SearchAnime/SearchAnime.jsx
@@ -1,8 +1,19 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Text, Input } from '@chakra-ui/react'
+import React, { useState, useEffect } from "react";
+
 export default function SearchAnime(props) {
+
+  const [searchedAnime, setSearchedAnime] = useState('');
+  const [animeResults, setAnimeResults] = useState([]);
+  
   return (
     <>
-      <h1>Test</h1>
+      <Box ht='10vh'>
+        <Box pt='64px'>
+          <Text>Search to add</Text>
+          <Input width='auto'/>
+        </Box>
+      </Box>
     </>
 )
 }
